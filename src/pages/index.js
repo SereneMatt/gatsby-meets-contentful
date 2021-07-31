406e6b4d974f4ad3295e7f36a930e6f0e7f45c0d
SereneMatt <serene1990@gmail.com>
2021-07-31 03:12:24 +0200

    Initialise Gatsby app

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+import { Layout, SEO } from "./components/index.js";
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>Hello Leute</h1>
+  </Layout>
+)
+
+export default IndexPage
