1962e20617fe4a6efbce4111a73483715c3770a4
SereneMatt <serene1990@gmail.com>
2023-02-09 01:29:01 -0600

    Switch to Node.js LTS

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,3 +7,27 @@ exports.onCreateWebpackConfig = ({ actions }) => {
     }
   })
 }
+
+exports.createPages = async ({ actions, graphql }) => {
+  const { data } = await graphql(`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            fields {
+              slug
+            }
+          }
+        }
+      }
+    }
+  `)
+  data.allMarkdownRemark.edges.forEach(edge => {
+    const slug = edge.node.fields.slug
+    actions.createPage({
+      path: slug,
+      component: require.resolve(`./src/templates/blog-post.js`),
+      context: { slug: slug },
+    })
+  })
+}
---
+++ src/components/Matt/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Matt = (): JSX.Element => <div>Hi, this is Serene</div>;
+
+export default Matt;
